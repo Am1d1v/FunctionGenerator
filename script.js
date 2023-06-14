9e69687cd33fcd501d4ab47ca6d6de5cb6bb7774
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-14 13:20:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,9 +10,31 @@ function* generator() {
 };
 const str = generator();
 
+//console.log(str.next().value);
+
+/*
+
 console.log(str.next());
 console.log(str.next());
 console.log(str.next());
 console.log(str.next());
 console.log(str.next());
 console.log(str.next());
+
+
+*/
+
+function* count(n){
+  for(let i = 0; i < n; i++){
+    yield i;
+  }
+}
+
+for (let k of count(10)){
+  console.log(k);
+}
+
+const counter = count(9);
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
